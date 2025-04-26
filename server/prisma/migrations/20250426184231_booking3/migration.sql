b168f1de34887a6faf5ff18a5c9f93427ea607e1
xtombrock <xtombrock1989@gmail.com>
2025-04-26 19:46:33 +0100

    save

---
+++ server/prisma/migrations/20250426184231_booking3/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `projectType` on the `ContactForm` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ContactForm" DROP COLUMN "projectType";
