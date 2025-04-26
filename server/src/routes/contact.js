9c8d3e3a092e17ed9bfa468cb32501411d362913
xtombrock <xtombrock1989@gmail.com>
2025-04-26 22:19:28 +0100

    save

--- server/src/routes/contact.js
+++ server/src/routes/contact.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import {
   createNewCallbackFormHandler,
   createNewContactFormHandler,
+  deleteAllCallbackFormsHandler,
+  deleteAllContactFormsHandler,
   deleteCallbackFormHandler,
   deleteContactFormHandler,
   getAllCallbackFormsHandler,
@@ -20,7 +22,7 @@ router.post('/create-new-contact-form', createNewContactFormHandler);
 router.post('/create-new-callback-form', createNewCallbackFormHandler);
 router.delete('/delete-contact-form/:formId', deleteContactFormHandler);
 router.delete('/delete-callback-form/:formId', deleteCallbackFormHandler);
-router.delete('/delete-all-contact-forms', deleteContactFormHandler);
-router.delete('/delete-all-callback-forms', deleteCallbackFormHandler);
+router.delete('/delete-all-contact-forms', deleteAllContactFormsHandler);
+router.delete('/delete-all-callback-forms', deleteAllCallbackFormsHandler);
 
 export default router;
