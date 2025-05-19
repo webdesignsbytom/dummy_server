9309903d0101f4a87af2d38b85e5365ca3a57dc9
xtombrock <xtombrock1989@gmail.com>
2025-05-19 16:24:16 +0100

    save

--- server/src/routes/newsletter.js
+++ server/src/routes/newsletter.js
@@ -15,8 +15,14 @@ const router = Router();
 
 router.get('/get-subscriber-list', getAllNewsletterSubscribersHandler);
 router.post('/subscribe-to-newsletter', subscribeToNewsletterHandler);
-router.delete('/delete-subscriber-by-id/:id', deleteNewsletterSubscriberByIdHandler);
-router.delete('/delete-subscriber-by-email/:email', deleteNewsletterSubscriberByEmailHandler);
+router.delete(
+  '/delete-subscriber-by-id/:id',
+  deleteNewsletterSubscriberByIdHandler
+);
+router.delete(
+  '/delete-subscriber-by-email/:email',
+  deleteNewsletterSubscriberByEmailHandler
+);
 router.delete('/delete-all-subscribers', deleteAllNewsletterSubscribersHandler);
 
 export default router;
