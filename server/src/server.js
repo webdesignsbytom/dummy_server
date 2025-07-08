0bc5a6d58ed3e94838a4bff0d61b999536e5311b
xtombrock <xtombrock1989@gmail.com>
2025-07-09 00:48:31 +0100

    save

--- server/src/server.js
+++ server/src/server.js
@@ -29,7 +29,7 @@ app.use(
   cors({
     origin: ['http://localhost:3000', 'https://dummy.bytetoast-studio.co.uk'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    credentials: false,
   })
 );
 
